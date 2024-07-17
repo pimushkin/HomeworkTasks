0930ad8713d4c2bcc50cf386a2750aed12577335
Dmitry Pimushkin <d.pimushkin@gmail.com>
2024-07-17 11:30:12 +0200

    Add greeting feature

--- Hw1/Ex1/Program.cs
+++ Hw1/Ex1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}!");
+        }
+    }
+}
